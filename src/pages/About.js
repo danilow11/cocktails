58429c71052e12a9ffed007f886264182a1ac7c5
daniel.alvarez <daniel.alvarez.m11@gmail.com>
2021-03-18 10:47:05 -0600

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis qui ad
+        voluptatum. Reiciendis architecto totam iusto minus, cumque tenetur ea
+        quibusdam numquam ad omnis adipisci nesciunt cupiditate voluptate error
+        repellendus ipsam facere amet deleniti animi nemo quidem enim maxime.
+        Ducimus.
+      </p>
+    </section>
+  );
+};
+
+export default About;
